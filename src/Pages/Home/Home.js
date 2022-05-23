4959901ef91ea3533130de5db8111fb8a81edd5b
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-05-23 06:31:10 +0600

    contact section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Footer from './../Shared/Footer';
+import Contact from './Contact';
 
 const Home = () => {
     return (
             // className='lg:px-20'
         <div>
             <Banner/>
+            <Contact/>
             <Footer/>
         </div>
     );
